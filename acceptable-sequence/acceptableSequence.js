88197970778d653a77e7a36d314810c8674dbd54
Chaz Kondo <Chaz@2ppgn4x.local>
2018-06-08 01:32:54 -1000

    Finished the first challenge

--- acceptable-sequence/acceptableSequence.js
+++ acceptable-sequence/acceptableSequence.js
@@ -1,3 +1,10 @@
 module.exports = function acceptableSequence(str) {
-  // write your code here
-}
+  for (var i = 0; i< str.length; i++) {
+    if (str[i].match(/[a-z]/i) 
+    && (str[i-1] !== "+" || str[i + 1] !== "+")){
+      return false;
+    }else{return true}
+  }
+ };
+
+   
