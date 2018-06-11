2cec6256c1d67852784a261752a2856c02db08b4
Chaz Kondo <Chaz@2ppgn4x.local>
2018-06-10 21:29:09 -1000

    Finished double string characters. Is there a way to do something x2?

--- double-str-chars/doubleStrChars.js
+++ double-str-chars/doubleStrChars.js
@@ -1,3 +1,26 @@
+// ## Challenge
+
+// You are given a string as an argument. Return a string in which each character is doubled. If any data type other than a string is passed as an argument, return a message that says `not a string!`.
+
+// Example:
+
+// ```
+// doubleStrChars('DevLeague') // returns 'DDeevvLLeeaagguuee'
+
+// doubleStrChars('Hello World!') // returns 'HHeelloo WWoorrlldd!!'
+
+// doubleStrChars(true) // returns 'not a string!'
+
+// doubleStrChars(2468) // returns 'not a string!'
+// ```
+
 module.exports = function doubleStrChars(str) {
-  // write your code in here
+  if(typeof str !== "string"){return 'not a string!'}else{
+    newStr = '';
+    for(var i=0;i<str.length;i++){
+      newStr += str[i];
+      newStr += str[i];
+    };
+  };
+  return newStr;
 }
