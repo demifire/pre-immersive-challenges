8acac2ba96769a08034fb60168c71693b588e5aa
Chaz Kondo <Chaz@2ppgn4x.local>
2018-06-10 19:12:14 -1000

    Finished the count by multiples - EZ

--- count-by-multiples/countByMultiples.js
+++ count-by-multiples/countByMultiples.js
@@ -1,3 +1,19 @@
+// ## Challenge
+
+// You are given two arguments: the `length` of elements that should be returned in an array with multiples of `x`. You must return the results as an array.
+
+// Example:
+
+// ```
+// countByMultiples(10, 2) // returns [2,4,6,8,10,12,14,16,18,20]
+
+// countByMultiples(5, 3) // returns [3,6,9,12,15]
+// ```
+
 module.exports = function countByMultiples(length, x) {
-  // write your code in here
+  arr = [];
+  for(i=1;i<length+1;i++){
+    arr.push(x*i)
+  };
+  return arr;
 }
