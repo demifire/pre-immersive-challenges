5eafad093643b536bbbc0ca48a8491415f8a9357
Chaz Kondo <Chaz@2ppgn4x.local>
2018-06-11 18:44:39 -1000

    Finished minimum split. Really?

--- minimum-splits/minimumSplits.js
+++ minimum-splits/minimumSplits.js
@@ -1,3 +1,17 @@
+// ## Challenge
+
+// Your task is to split a candy bar of given dimensions n x m into small squares. The size of each square is 1x1 and unbreakable. Return the minimum number of splits needed to break the candy bar into single squares.
+
+// For example if you are given a 2x1 candy bar you can split it to single squares in just one break, but for size 3x1 you must do two breaks.
+
+// Example:
+
+// ```
+// minimumSplits(3, 8) // returns 23
+
+// minimumSplits(1, 1) // returns 0
+// ```
+
 module.exports = function minimumSplits(n, m) {
-  // write your code in here
+  return (m*n)-1;
 }
