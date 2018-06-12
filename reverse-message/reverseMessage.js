c199407b42c75ac700d3c86f5ab1826f76123710
Chaz Kondo <Chaz@2ppgn4x.local>
2018-06-11 21:16:38 -1000

    Skipped over the reverse message. Finished swapped letter case

--- reverse-message/reverseMessage.js
+++ reverse-message/reverseMessage.js
@@ -1,3 +1,17 @@
+// ## Challenge
+
+// You are given a string as an argument. Reverse the string so that the words and letters passed into it are made lower case and reversed. In addition, capitalize the first letter of the newly reversed words. If a number or symbol(!#,>) is now in the first position of the word, no capitalization needs to occur.
+
+// Example:
+
+// ```
+// reverseMessage('This is an example of a Reversed Message!'); // returns '!egassem Desrever A Fo Elpmaxe Na Si Siht'
+// ```
+
 module.exports = function reverseMessage(str) {
-  // write your code in here
-}
+  var lul = str.split("").reverse().join("");
+  console.log(lul);
+  lul = lul.toLowerCase().replace(/\b[a-z](?=[a-z]{2})/g, function(letter) {
+    return letter.toUpperCase(); } );
+    return lul
+};
--- swap-letter-case/swapLetterCase.js
+++ swap-letter-case/swapLetterCase.js
@@ -1,3 +1,31 @@
+// ## Challenge
+
+// You are given a string as an argument. You need to return a string with swapped letter casing. If a letter is capitalized make it lowercase, and vice versa. If the character given is not a letter, leave it alone.
+
+// Example:
+
+// ```
+// swapLetterCase('Tea tiMe'); // returns 'tEA TImE';
+
+// swapLetterCase('feed ME'); // returns 'FEED me';
+// ```
+
+
 module.exports = function swapLetterCase(str) {
-  // write your code in here
+  newString = '';
+  for (var i = 0; i<str.length; i++) {
+    if (str[i] === str[i].toUpperCase()
+        && str[i] !== str[i].toLowerCase()) {
+        console.log(str[i] + ": " + true);
+        newString += str[i].toLowerCase();
+    } else if (str[i] === str[i].toLowerCase()
+    && str[i] !== str[i].toUpperCase()) {
+    console.log(str[i] + ": " + false);
+    newString += str[i].toUpperCase();
+} else {console.log(str[i] + " is not a letter.");
+     newString += str[i];
+    }
+  }
+  console.log(newString)
+  return newString;
 }
