8602e3e83bb6d7b2791f6c0cc2ac0c98ba79be86
Chaz Kondo <Chaz@2ppgn4x.local>
2018-06-11 20:04:40 -1000

    Finished needle in the haystack

--- needle-in-haystack/needleInHaystack.js
+++ needle-in-haystack/needleInHaystack.js
@@ -1,3 +1,21 @@
+// ## Challenge
+
+// You are given an array of strings as an argument. Find the word `"needle"` in the array of "haystacks" and return it's index. If `"needle"` does not exist in the array return `false`.
+
+// Example:
+
+// ```
+// needleInHaystack(['haystack', 'needle', 'haystack']) // returns 1
+
+// needleInHaystack(['haystack', 'haystack', 'haystack', 'haystack']) // returns false
+// ```
+
 module.exports = function needleInHaystack(haystack) {
-  // write code in here
+  for(var i=0;i<haystack.length;i++){
+    if(haystack[i] === 'needle'){
+      var needle = i;
+      return needle;
+    }else{}
+  }
+  return false;
 }
