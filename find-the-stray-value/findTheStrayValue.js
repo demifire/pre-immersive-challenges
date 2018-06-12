b2229f4aa14c77800fa9119969c0f1df68083577
Chaz Kondo <Chaz@2ppgn4x.local>
2018-06-11 17:33:25 -1000

    Find stray value. EZ

--- find-the-stray-value/findTheStrayValue.js
+++ find-the-stray-value/findTheStrayValue.js
@@ -1,3 +1,20 @@
+// ## Challenge
+
+// You are given an array in which all of the values are the same except for one stray value. You need to return the stray value.
+
+// Example:
+
+// ```
+// findTheStrayValue(['woot', 'woot', 'nope', 'woot', 'woot']) // returns 'nope'
+
+// findTheStrayValue([2, 2, 9, 2, 2, 2, 2]) // returns 9
+// ```
+
 module.exports = function findTheStrayValue(array) {
-  // write your code in here
+  for (var i=0;i<array.length;i++){
+    if(array[i] !== array[0]){
+      var result = array[i];
+    }else{};
+  };
+  return result;
 }
