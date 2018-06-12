d79f043cb43b9911065a50626bd84c687cce0c52
Chaz Kondo <Chaz@2ppgn4x.local>
2018-06-11 17:19:33 -1000

    Finished the find longest word. Pretty ez

--- find-the-longest-word/findTheLongestWord.js
+++ find-the-longest-word/findTheLongestWord.js
@@ -1,3 +1,26 @@
+// ## Challenge
+
+// You are given an array of strings as an argument. Return the longest word in the array.
+
+// Example:
+
+// ```
+// findTheLongestWord( ['more', 'coffee', 'pleaseeeeee']) // returns 'pleaseeeeee'
+
+// findTheLongestWord( ['to', 'the', 'moon']) // returns 'moon'
+// ```
+
 module.exports = function findTheLongestWord(sen) {
-  // write your code in here
+  var x = 0;
+  var index = 0;
+  for(var i=0;i<sen.length;i++){
+    for(var j=0;j<sen[i].length;j++){
+      if(sen[i].length>x){
+        x=sen[i].length;
+        console.log(sen[i]);
+        index = i;
+      }else{};
+    }
+  }
+  return sen[index];
 }
