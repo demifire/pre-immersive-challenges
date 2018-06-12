493c094cd32d7a99528db036a79b45b243761379
Chaz Kondo <Chaz@2ppgn4x.local>
2018-06-11 16:38:37 -1000

    Find middle number done. Not that hard

--- find-middle-value/findMiddleValue.js
+++ find-middle-value/findMiddleValue.js
@@ -11,7 +11,25 @@
 // ```
 
 module.exports = function findMiddleValue(numArray) {
+  var x = [0];
   for(var i=0;i<numArray.length;i++){
-    if()
+    if(numArray[i]>x[0]){
+      x[0]=numArray[i];
+      var bigNum = x[0];
+    }
   }
+  console.log(bigNum);
+  for(var i=0;i<numArray.length;i++){
+    if(numArray[i]<x[0]){
+      x[0]=numArray[i];
+      var smallNum = x[0];
+    }
+  }
+  console.log(smallNum);
+  for(var i=0;i<numArray.length;i++){
+    if(numArray[i]>smallNum && numArray[i]<bigNum){
+      var midNumIndex = i;
+    }
+  }
+  return midNumIndex;
 }
