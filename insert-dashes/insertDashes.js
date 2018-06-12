e46c2570cd1371b0725a1014ab4ec0b172ff1d1f
Chaz Kondo <Chaz@2ppgn4x.local>
2018-06-11 18:39:30 -1000

    Finished dashes

--- insert-dashes/insertDashes.js
+++ insert-dashes/insertDashes.js
@@ -1,3 +1,21 @@
+// ## Challenge
+
+// You are given a **number** as an argument. You need to return a **string** with dashes between even numbers.
+
+// Example:
+
+// ```
+// insertDashes(553847) // returns '5538-47'
+
+// insertDashes(2468) // returns '2-4-6-8'
+// ```
+
 module.exports = function insertDashes(num) {
-  // write your code in here
+  var arr = num.toString(10).split("").map(Number);
+  for (var i=0;i<arr.length;i++){
+    if(arr[i]%2 === 0 && arr[i+1]%2 === 0){
+      arr.splice(i+1, 0, "-");
+    }else{}
+  }
+  return arr.join('');
 }
