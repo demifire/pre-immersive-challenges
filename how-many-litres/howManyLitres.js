8244c58556c553aecf379df5a342d8724f5792e0
Chaz Kondo <Chaz@2ppgn4x.local>
2018-06-11 18:05:22 -1000

    half life was done. Maybe I'll come back to that? Finished how many litres

--- how-many-litres/howManyLitres.js
+++ how-many-litres/howManyLitres.js
@@ -1,3 +1,16 @@
+// ## Challenge
+
+// In order to stay hydrated, you need to drink 0.5 litres of water per hour. You are given amount of hours as an argument. Return the number of litres you will drink, rounded down to the smallest whole value.
+
+// Example:
+
+// ```
+// howManyLitres(5) // returns 2
+
+// howManyLitres(9) // returns 4
+// ```
+
+
 module.exports = function howManyLitres(hours) {
-  // write your code in here
+  return Math.floor(0.5*hours)
 }
