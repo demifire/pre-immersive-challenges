8ce6575597b7b3db962090316c28f9ecd4e38e33
Chaz Kondo <Chaz@2ppgn4x.local>
2018-06-11 19:58:31 -1000

    Finished the zero moving stuff

--- move-the-zeros/moveTheZeros.js
+++ move-the-zeros/moveTheZeros.js
@@ -1,3 +1,39 @@
+// ## Challenge
+
+// You are given two arguments, an array of numbers and a boolean value `isRight` that defaults to true. Return the array with all the zeros to the right of the array if `isRight` is true, otherwise move all the zeros to the left if `isRight` is false. The order of the other numbers should not change.
+
+// Example:
+
+// ```
+// moveTheZeros([5, 0, 0, 6, 1], false) // returns [0, 0, 5, 6, 1]
+
+// moveTheZeros([1, 2, 3, 0, 0, 4, 5, 0], true) // returns  [1, 2, 3, 4, 5, 0, 0, 0]
+// ```
+
 module.exports = function moveTheZeros(arrNum, isRight) {
-  // write your code here
+  var count = 0;
+  var array = [];
+  if(isRight === true){
+    for(var i=0;i<arrNum.length;i++){
+      if(arrNum[i] === 0){
+        count++;
+      }else{array.push(arrNum[i]);}
+    }
+    for(var i=0;i<count;i++){
+      array.push(0);
+    }
+  }
+  else{
+    for(var i=0;i<arrNum.length;i++){
+      if(arrNum[i] === 0){
+        array.push(0)
+      }else{}
+    }
+    for(var i=0;i<arrNum.length;i++){
+      if(arrNum[i] !== 0){
+        array.push(arrNum[i])
+      }
+    }
+  }
+  return array
 }
